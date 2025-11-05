51431f65b7e389508d95c3b7fea184fe88b39aa3
[dyad] <mariamraouf2002@gmail.com>
2025-11-05 03:08:22 +0200

    [dyad] Fixed footer logo visibility in Arabic by applying a more effective CSS filter to the image. - wrote 1 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,8 +23,8 @@ const Footer = () => {
               <img 
                 src="/sidra-med-logo.png" 
                 alt="Sidra Med Logo" 
-                className="w-24 h-24 object-contain filter invert(100%) sepia(100%) saturate(0%) hue-rotate(200deg) brightness(1000%)" 
-              /> {/* Corrected filter to make it white */}
+                className="w-24 h-24 object-contain filter brightness(0) invert(1)" 
+              /> {/* Simplified filter to make it white */}
             </div>
             <p className="text-gray-300 mb-8 leading-relaxed text-lg">
               {t('empoweringHealthcare')}
